da8102a6574bc57e019a73317bd5521534ca8ac7
Andy Theuninck <andy@gohanman.com>
2013-07-23 12:42:14 -0500

    BUGFIX: json.dumps can deal with a list, jsonify cannot

--- CoreWebApi/json_out.py
+++ CoreWebApi/json_out.py
@@ -2,6 +2,8 @@ from CoreWebApi import app
 from flask import json, jsonify
 
 def json_as_configured(dict):
+	if type(dict) == type([]):
+		dict = { 'results' : dict }
 	if app.config.has_key('JSON_HEADERS') and app.config['JSON_HEADERS']:
 		return jsonify(dict)
 	else:
